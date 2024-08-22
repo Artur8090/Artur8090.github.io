b2c15939296f467ca8107dc4ecfa781496a401cb
Artur8090 <artur.cheuchiev@gmail.com>
2024-08-22 19:51:07 +0200

    visual changes, animation, + dark/light mode in progress

---
+++ mode.js
@@ -0,0 +1,5 @@
+const settingChange = document.querySelector('#mode');
+settingChange.addEventListener('click',function(){
+    document.querySelector('body').style.backgroundColor = '#F9F6EE';
+    document.querySelector('body').style.color = '#F9F6EE';
+})
