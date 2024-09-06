c44c5be4329efa4647a5c9fb1880d7cc82a2c288
artur8090 <artur.cheuchiev@gmail.com>
2024-09-06 13:05:08 +0200

    updated background

--- mode.js
+++
@@ -1,5 +0,0 @@
-const settingChange = document.querySelector('#mode');
-settingChange.addEventListener('click',function(){
-    document.querySelector('body').style.backgroundColor = '#F9F6EE';
-    document.querySelector('body').style.color = '#F9F6EE';
-})
